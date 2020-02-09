fc6e327d5cb661ce9cd7a234e203ebf769741005
Francis Bourgouin <netbeuix@gmail.com>
2020-02-09 10:09:46 -0500

    Added form & point mechanic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.scss";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import { ReactComponent as StopMtlLogo } from "../stopmtl-logo.svg";
 
 function App() {
-  const lang = "fr";
-  const position = [45.53, -73.57];
+  const [position, setPosition] = useState([45.53, -73.57]);
+  const [mapPoints, setMapPoints] = useState([])
+  const [newPoint, setNewPoint] = useState({
+      active:false,
+      lat:undefined,
+      lng:undefined,
+      date:undefined,
+      moment:undefined,
+      age:undefined,
+      genre:undefined,
+      groupe_ethnique:undefined,
+      orientation_sexuelle:undefined,
+  })
+  
+  const momentDuJour = ['matin','midi','soir'].map(ele => <option value={ele}>{ele}</option>)
+  const ageAuMoment = [...Array(87).keys()].map(ele => <option value={ele+12}>{ele+12}</option>)
+  const genre = ['femme','homme','Ces catégories ne me définissent pas'].map(ele => <option value={ele}>{ele}</option>)
+  const groupeEthnique = ["Noir","Arabe","Autochtone","Sud-Asiatique","Chinois","Philippin","Asiatique du Sud-Est (p. ex., Vietnamien, Cambodgien, Malaisien, Laotien)","Asiatique occidental (p. ex., Iranien, Afghan)","Coréen","Japonais","Latino-Américain","Blanc","Autre – précisez"].map(ele => <option value={ele}>{ele}</option>)
+  const orientationSexuelle = ["Hétérosexuel(le)","Gai","Lesbienne","Bisexuel(le)","Ces catégories ne me définissent pas"].map(ele => <option value={ele}>{ele}</option>)
+
+    const addMapPoint = event => {
+    const {lat, lng} = event.latlng
+    
+    setNewPoint({...newPoint, lat, lng, active:true})
+  }
+  const updateMapPoint = event => {
+    const {name, value} = event.target
+    setNewPoint({...newPoint, [name]:value})
+  }
+
+  const saveMapPoint = () => {
+    //Save to mapPointList
+  }
   const map = (
-    <Map center={position} zoom={11}>
+    <Map center={position} zoom={11} onClick={addMapPoint}>
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={position}>
+      {newPoint.active &&
+      <Marker position={[newPoint.lat, newPoint.lng]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+}
     </Map>
   );
 
@@ -37,7 +70,27 @@ function App() {
         </ul>
       </header>
       <main>
+        <section className="map">
         {map}
+        {newPoint.active && (
+          <div className="question_form">
+            <input type="date" name="date" onChange={e => updateMapPoint(e)}/>
+            <select name="moment" onChange={e => updateMapPoint(e)}>{momentDuJour}            </select>
+            <select name="age" onChange={e => updateMapPoint(e)}>
+              {ageAuMoment}
+            </select>
+            <select name="genre" onChange={e => updateMapPoint(e)}>
+              {genre}
+            </select>
+            <select name="groupe_ethnique" onChange={e => updateMapPoint(e)}>
+{groupeEthnique}
+            </select>
+            <select name="orientation_sexuelle" onChange={e => updateMapPoint(e)}>
+{orientationSexuelle}
+            </select>
+          </div>
+        )}
+          </section>
         <section className='about'>
           <h2>Qu'est-ce que StopMTL ?</h2>
           <p>
